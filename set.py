3d058c7011abe60956f73128067473d2a107e347
Chris <chris.field@btinternet.com>
2023-12-18 23:49:12 +0000

    Modbus slave objects now contained within Point, Axlecounter, Signal and Plunger objects. Re-focused on triggers rather than routes. Included network in object definitions and in config file.

--- set.py
+++ set.py
@@ -2,22 +2,25 @@
 def set_point(point, direction):
     if direction == "normal":
         try:
-            point.slave.write_bit(self.reverse_coil, 0)
-            point.slave.write_bit(self.normal_coil, 1)
+            point.slave.write_bit(point.reverse_coil, 0)
+            point.slave.write_bit(point.normal_coil, 1)
             return True
         except ValueError:
             status = "Comms failure" # add details and logging
             return False
     if direction == "reverse":
         try:
-            point.slave.write_bit(self.normal_coil, 0)
-            point.slave.write_bit(self.reverse_coil, 1)
+            point.slave.write_bit(point.normal_coil, 0)
+            point.slave.write_bit(point.reverse_coil, 1)
             return True
         except ValueError:
             status = "Comms failure" # add details and logging
             return False
 
-def  set_route(route):
+def set_signal(signal, aspect):
+    pass
+
+def set_route(route):
     # check route is clear to set
     # return "not_available" if not available
     # set points
@@ -25,3 +28,7 @@ def  set_route(route):
     # set signals
     # return "set" once points and signals set
     pass
+
+def set_plungers_clear(all_plungers_dict):
+    for plunger_key, plunger in all_plungers_dict.items():
+        plunger.status = 0
